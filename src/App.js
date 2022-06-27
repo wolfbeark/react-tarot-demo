63f964d965cc26a3d21e218cb00e3090a7e22ed0
wolfbeark <wolfbeark0327@gmail.com>
2022-06-27 19:55:36 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 
 
@@ -13,7 +13,6 @@ import FourthComponent from './Components/FourthComponent/FourthComponent';
 import DragTest from './Components/DragTest/DragTest';
 import DragTest2 from './Components/DragTest2/DragTest2';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 function App() {
@@ -27,7 +26,8 @@ function App() {
       //     <Route exact path='/drag2' element={<DragTest2 />} />
       //   </Routes>
       // </div>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <div className='App'> 
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route exact path='/' element={<MainComponent />}/>
           
@@ -35,7 +35,9 @@ function App() {
           <Route exact path='/drag' element={<DragTest />} />
           <Route exact path='/drag2' element={<DragTest2 />} />
         </Routes>
-      </BrowserRouter>
+       </BrowserRouter>
+      </div>
+      
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -22,7 +22,7 @@ const createStoreWithMiddleware =
   applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 
-//const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         {/* //<BrowserRouter basename={process.env.PUBLIC_URL}> */}
