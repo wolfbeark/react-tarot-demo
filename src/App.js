336471cbc993284f752378e43603868772aa15c5
wolfbeark <wolfbeark0327@gmail.com>
2022-06-27 19:45:17 +0900

    test1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import ReactDOM from 'react-dom/client';
 
 
 
@@ -12,10 +13,21 @@ import FourthComponent from './Components/FourthComponent/FourthComponent';
 import DragTest from './Components/DragTest/DragTest';
 import DragTest2 from './Components/DragTest2/DragTest2';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
 
 function App() {
   return (
-      <div className="App" >
+      // <div className="App" >
+      //   <Routes>
+      //     <Route exact path='/' element={<MainComponent />}/>
+          
+      //     <Route exact path='/fourth' element={<FourthComponent />} />
+      //     <Route exact path='/drag' element={<DragTest />} />
+      //     <Route exact path='/drag2' element={<DragTest2 />} />
+      //   </Routes>
+      // </div>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route exact path='/' element={<MainComponent />}/>
           
@@ -23,7 +35,7 @@ function App() {
           <Route exact path='/drag' element={<DragTest />} />
           <Route exact path='/drag2' element={<DragTest2 />} />
         </Routes>
-      </div>
+      </BrowserRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -22,15 +22,15 @@ const createStoreWithMiddleware =
   applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        {/* //<BrowserRouter basename={process.env.PUBLIC_URL}> */}
           <Provider store={createStoreWithMiddleware(Reducer,
             composeWithDevTools())}>
             <App />
           </Provider>
-        </BrowserRouter>
+        {/* /*</BrowserRouter>*/ }
     </React.StrictMode>
 );
 
